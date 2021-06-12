9197b5cb01dcad8b33e057dfcd8fc097320d50dd
Rimarok <romaric.ruga@gmail.com>
2021-06-12 08:03:11 +0200

    add FoundationStoneSvg + integration in App

--- front/components/App/App.tsx
+++ front/components/App/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Explorer } from "../Explorer";
 import { useDirentContent } from "./useDirentContent";
+import { FoundationStoneSvg } from "../_images/FoundationStoneSvg";
 
 export function App() {
   const { dirent, setDirent, direntContent } = useDirentContent();
   return (
     <div>
+      <FoundationStoneSvg style={{ height: "256px", width: "256px" }} />
       <h1>Welcome aboard the Code Scryer !</h1>
       <Explorer
         dirent={dirent}
---
+++ front/components/_images/FoundationStoneSvg.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export function FoundationStoneSvg({ style }: { style?: React.CSSProperties }) {
+  return (
+    <svg
+      width="210mm"
+      height="210mm"
+      viewBox="0 0 210 210"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      style={style}
+    >
+      <path
+        style={{ fill: "#c8beb7", strokeWidth: "0.264583px" }}
+        d="m 184.19711,185.26803 c 0,0 19.34159,-159.975677 -79.24759,-159.209137 C 13.292109,26.771538 34.26923,187.40986 34.26923,187.40986 Z"
+      />
+      <path
+        style={{ fill: "#00ffab", strokeWidth: "0.247565px" }}
+        d="M 77.509411,64.277679 133.27672,63.05875 104.8803,106.84606 Z"
+      />
+      <path
+        style={{ fill: "#917c6f", strokeWidth: "0.312253px" }}
+        d="m 56.146539,132.9549 100.607101,-2.13458 -0.41573,8.53834 -100.607103,0.85383 z"
+      />
+      <path
+        style={{ fill: "#ac9d93", strokeWidth: "0.283437px" }}
+        d="m 82.221193,155.60361 0.04105,-7.65392 64.186357,0.60376 0.72664,6.3394 z"
+      />
+    </svg>
+  );
+}
